61f58571649cd3d64500c1f6eade26ca39030db4
Eznamen <111395043+Eznamen@users.noreply.github.com>
2022-09-18 20:36:22 +0300

    изменение работы и создания Set

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -4,17 +4,19 @@ import java.util.Set;
 public class WordsChecker {
 
     private String text;
+    Set<String> set = new HashSet<>();
 
     public WordsChecker(String text) {
         this.text = text;
+
+        for (String s : text.split("\\P{IsAlphabetic}+")) {
+            set.add(s);
+            s = s.toLowerCase();
+            set.add(s);
+        }
     }
 
     public boolean hasWord(String word) {
-        String textLower = text.toLowerCase();
-        Set<String> set = new HashSet<>();
-        for (String s : textLower.split("\\P{IsAlphabetic}+")) {
-            set.add(s);
-        }
         return set.contains(word);
     }
 }
